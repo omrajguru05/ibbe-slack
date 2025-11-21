ef5f5747a2e67c9a50f017cde27a10ff347b838f
omrajguru05 <omrajguru077@gmail.com>
2025-11-21 14:57:36 +0530

    Fix: Add dynamic export to /chat page to prevent Vercel prerender error

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from 'next/navigation'
 
+export const dynamic = 'force-dynamic'
+
 export default function ChatPage() {
     // Redirect to general channel by default
     redirect('/chat/general')
